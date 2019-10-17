fab7de54b542fe6f31ebcddf7454b09b5da4a6ff
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-10-17 14:43:25 -0500

    wip

--- Instructor.cs
+++ Instructor.cs
@@ -1,11 +1,7 @@
 namespace StudentExercises
 {
-    class Instructor
+    public class Instructor : NSSPerson
     {
-       public string FirstName { get; set; } 
-       public string LastName { get; set; } 
-       public string SlackHandle { get; set; }
-       public Cohort Cohort { get; set; }
        public string Specialty { get; set; }
        public void AssignExercise(Student student, Exercise exercise)
        {
--- Student.cs
+++ Student.cs
@@ -2,12 +2,8 @@ using System.Collections.Generic;
 
 namespace StudentExercises
 {
-    class Student
+    public class Student : NSSPerson
     {
-       public string FirstName { get; set; } 
-       public string LastName { get; set; } 
-       public string SlackHandle { get; set; }
-       public Cohort Cohort { get; set; }
        public List<Exercise> exercises = new List<Exercise>();
     }
 }
